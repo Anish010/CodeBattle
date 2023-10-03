a3a505e3851f88316918148ef2be42a636d989fd
Anish010 <anishmohanty101@gmail.com>
2023-10-03 22:40:53 +0530

    deploy and build

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectDatabase = () => {
+    
     mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then((data) => {
         console.log(`Mongodb connected with server : ${data.connection.host}`);
     })
